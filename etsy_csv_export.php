8eba5ab12537574c9bbdd5337fe15a5df6f8df94
Van Aldrin Sarzate <vanaldrinsarzate@gmail.com>
2015-01-26 22:17:44 +0800

    Update etsy_csv_export.php

--- etsy_csv_export.php
+++ etsy_csv_export.php
@@ -1,33 +1,31 @@
-<?php
-/*
- This file will generate our CSV table. There is nothing to display on this page, it is simply used
- to generate our CSV file and then exit. That way we won't be re-directed after pressing the export
- to CSV button on the previous page.
-*/
-//First we'll generate an output variable called out. It'll have all of our text for the CSV file.
-$out = '';
-//Next let's initialize a variable for our filename prefix (optional).
-$filename_prefix = 'csv';
-//Next we'll check to see if our variables posted and if they did we'll simply append them to out.
-if (isset($_POST['csv_hdr'])) {
-$out .= $_POST['csv_hdr'];
-$out .= "\n";
-}
-if (isset($_POST['csv_output'])) {
-$out .= $_POST['csv_output'];
-$out .= "\n";
-}
-//Now we're ready to create a file. This method generates a filename based on the current date & time.
-$filename = $filename_prefix."_".date("Y-m-d_H-i",time());
-//Generate the CSV file header
-header("Content-type: application/vnd.ms-excel");
-header("Content-Encoding: UTF-8");
-header("Content-type: text/csv; charset=UTF-8");
-header("Content-disposition: csv" . date("Y-m-d") . ".csv");
-header("Content-disposition: filename=".$filename.".csv");
-echo "\xEF\xBB\xBF"; // UTF-8 BOM
-//Print the contents of out to the generated file.
-echo $out;
-//Exit the script
-exit;
-?>
+<!DOCTYPE html><head>
+<meta charset="utf-8">
+<title>Orders Table</title>
+<script src="jquery-1.9.0.min.js"></script>
+<script>
+$(document).ready(function(e) {
+    $("button").on('click',function(e){
+		if($('input[type=file]').val()!=""){
+			var file = $('input[type=file]')[0].files[0].name;
+			var ext = file.split('.').pop().toLowerCase();
+			if($.inArray(ext, ['csv']) == -1) {
+				alert('invalid extension!');	
+			}else{
+				window.location = 'upload.php?csv='+file;	
+			}
+					
+		
+		}
+		return false;	
+	});
+});
+</script>
+</head>
+
+
+<input type="file" name="csv" value="" />
+<button>Export now!</button>
+
+
+
+ 
